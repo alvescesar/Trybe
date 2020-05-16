79c60111dab822aa56a3956639f2ee85278c2d8e
César Alves <alvescesar@hotmail.com>
2020-05-16 17:40:52 -0300

    Revised 4_2/exercise_06

--- exercises/4_2/exercise_06.js
+++ exercises/4_2/exercise_06.js
@@ -2,9 +2,13 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let oddAmount = 0;
 
 for (i = 0; i < numbers.length; i += 1) {
-	if (numbers[i] % 2 != 0) {
-		console.log(numbers[i]);
+	if (numbers[i] % 2 !== 0) {
+		oddAmount += 1;
+	} else if (numbers[i] % 2 === 0) {
+		oddAmount == 0;
 	} else {
-		console.log("Found no odd value.")
+		console.log("Found no odd value.");
 	}
 }
+
+console.log(oddAmount);
