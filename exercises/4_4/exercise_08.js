04918bc472bf32604375f9dafc158f19bb70bc22
César Alves <alvescesar@hotmail.com>
2020-05-17 22:11:53 -0300

    Added exercise_08

--- exercises/4_4/exercise_08.js
+++ exercises/4_4/exercise_08.js
@@ -1,12 +1,12 @@
 let test = [23, 67, 9, 1, 105, 98, 340, 32];
 
 function arrayIndexCounter(array) {
-	let highest = 0;
+	let lowest = array[0];
 	let counter = 0;
 
 	for (let index in array) {
-		if (array[index] > highest) {
-			highest = array[index];
+		if (array[index] < lowest) {
+			lowest = array[index];
 			counter = index;
 		}
 	}
