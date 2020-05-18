1666da4bc6bdcbbab99ad3c1c973e57fc25cf03c
César Alves <alvescesar@hotmail.com>
2020-05-18 01:45:14 -0300

    Exercise_10 new attempt

--- exercises/4_4/exercise_10.js
+++ exercises/4_4/exercise_10.js
@@ -1,17 +1,24 @@
 let test = [2, 3, 2, 5, 8, 2, 3];
 
 function checkRepeated(numbers) {
-    let repeated = numbers[0];
+    let repeated = 0;
     let counter = 0;
+    let repeatedIndex = 0;
 
     for (let index in numbers) {
-        if (numbers[index] === repeated) {
-            repeated = numbers[index];
-            counter = index;
+        let numberChecker = numbers[index];
+
+        for (let index2 in numbers) {
+            if (numberChecker === numbers[index2]) {
+                counter += 1;
+            }
+        }
+        if (counter > repeated) {
+            repeated = counter;
+            repeatedIndex = index;
         }
     }
-
-    console.log(counter);
+    console.log(repeatedIndex);
 }
 
 checkRepeated(test);
