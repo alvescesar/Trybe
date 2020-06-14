80fea9322916e13fdcee7d4704cac8112aaeeb32
César Alves <alvescesar@hotmail.com>
2020-06-14 04:13:34 -0300

    Finished functions 7 && 8

--- exercises/7_2/exercise_01.js
+++ exercises/7_2/exercise_01.js
@@ -49,3 +49,18 @@ const studentCounter = (object, key) => {
   return counter;
 }
 console.log(studentCounter(allLessons, 'numeroEstudantes'));
+
+//Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto.
+const listValue = (object, index) => {
+  let position = Object.values(object)[index];
+  return position;
+}
+console.log(listValue(lesson1, 2));
+
+//Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave
+
+const verifyEntry = (object, key, value) => {
+  if (object[key] === value) return true; else return false;
+}
+
+console.log(verifyEntry(lesson1, 'materia', 'Matemática'));
