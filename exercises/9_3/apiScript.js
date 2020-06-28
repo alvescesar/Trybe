bc50f39d98ceafd29fc9e6869a14da2faa60e310
César Alves <alvescesar@hotmail.com>
2020-06-28 02:02:31 -0300

    Corrected exercises

--- exercises/9_3/apiScript.js
+++ exercises/9_3/apiScript.js
@@ -15,32 +15,25 @@ const fetchJoke = () => {
 
 /*----------Next Exercise---------- */
 
-function randomNumbers() {
-  return new Promise((resolve, reject) => {
-  const numbersArray = [];
-  
-  for(let i = 0; i < 10; i += 1) {
-    numbersArray.push(Math.floor(Math.random() * 50) ** 2);
-  }
-  const arraySum = numbersArray.reduce((acc, result) => acc + result);
+const fetchPromise = () => {
+  const myPromise = new Promise((resolve, reject) => {
+    const numbersArray = [];
+    
+    for(let i = 0; i < 10; i += 1) {
+      numbersArray.push(Math.floor(Math.random() * 50) ** 2);
+    }
 
-  (arraySum < 8000) ? resolve(divideAll(arraySum)) : reject('É mais de oito mil! Essa promise deve estar quebrada!');
+    const arraySum = numbersArray.reduce((acc, result) => acc + result);
+
+    (arraySum < 8000) ? resolve(arraySum) : reject();
   });
-}
 
-function divideAll(sum) {
-  return new Promise((resolve, reject) => {
-    const arrayDivide = [2, 3, 5, 10];
-    const division = arrayDivide.map(numbers => sum / numbers);
+  myPromise
+    .then(sum => [2, 3, 5, 10].map(number => sum / number))
+    .then(array => array.reduce((number, acc) => number + acc, 0))
+    .catch(() =>
+      console.log('É mais de oito mil! Essa promise deve estar quebrada!')
+    );
+};
 
-    (division) ? resolve(division) : reject('No deal');
-  });
-}
-
-function sumAll(result) {
-  return new Promise((resolve, reject) => {
-    const sumResult = result.reduce((acc, number) => acc + number);
-
-    (sumResult) ? resolve(console.log(sum)) : reject('No deal');
-  });
-}
+fetchPromise();
