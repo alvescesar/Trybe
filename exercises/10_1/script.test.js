5c7f3cb120d400da9c55ed59fd022a81d49ac6b4
César Alves <alvescesar@hotmail.com>
2020-07-06 23:15:59 -0300

    Solved exercise 02 'getUserName'

--- exercises/10_1/script.test.js
+++ exercises/10_1/script.test.js
@@ -10,3 +10,42 @@ test('text should be all uppercase', async () => {
 });
 
 // exercise_02
+const users = {
+  4: { name: 'Mark' },
+  5: { name: 'Paul' }
+  };
+  
+  const findUserById = (id) => {
+    return new Promise((resolve, reject) => {
+        if (users[id]) {
+          return resolve(users[id]);
+        }
+  
+        return reject({ error: `User with ${id} not found.` });
+    });
+  }
+  
+  const getUserName = (userID) => {
+    return findUserById(userID).then(user => user.name);
+  }
+
+  describe('Get the user`s name', () => {
+    describe('If the user exists', () => {
+      it('returns a name', async () => {
+        expect.assertions(1);
+        const testUser = await getUserName(4);
+        expect(testUser).toEqual('Mark');
+      });
+    });
+
+    describe('if the user does not exists', () => {
+      it('returns an error', async () => {
+        expect.assertions(1);
+        try {
+          await getUserName(1);
+        } catch (error) {
+          expect(error).toEqual({ error: 'User with 1 not found.'});
+        }
+      });
+    });
+  });
