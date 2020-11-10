1fb1549aba0d524d1c92b1a862883e2b7bb4b33a
César Alves <alvescesar@hotmail.com>
2020-11-10 20:22:57 -0300

    Exercise 02

--- exercises/26_1/index.js
+++ exercises/26_1/index.js
@@ -6,9 +6,37 @@ function findIMC() {
 
   console.log('Weight: %s, Height: %s', weight, height);
 
-  const imc = (weight / Math.pow(height, 2)).toFixed(2);
+  const bmi = (weight / Math.pow(height, 2)).toFixed(2);
 
-  console.log('IMC: %s', imc);
+  console.log('BMI: %s', bmi);
+
+  if (bmi < 18.5) {
+    console.log('Underweight');
+    return;
+  }
+  
+  if (bmi >= 18.5 && bmi < 25) {
+    console.log('Normal weight');
+    return;
+  }
+  
+  if (bmi >= 25 && bmi < 30) {
+    console.log('Overweight');
+    return;
+  }
+  
+  if (bmi >= 30 && bmi < 35) {
+    console.log('Obese');
+    return;
+  }
+  
+  if (bmi >= 35 && bmi < 40) {
+    console.log('Severely obese');
+    return;
+  }
+  
+  console.log('Morbidly obese');
+
 }
 
 findIMC();
