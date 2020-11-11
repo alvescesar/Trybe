da33a8e4bebafff851076aebff26fe43b75cc241
César Alves <alvescesar@hotmail.com>
2020-11-10 22:15:06 -0300

    Exercise 03

--- exercises/26_1/index.js
+++ exercises/26_1/index.js
@@ -1,8 +1,14 @@
-const readline = require('readline-sync');
+// const readline = require('readline-sync');
+const inquire = require('inquirer');
 
-function findIMC() {
-  const weight = readline.questionFloat('How much do you weight? ').toFixed(2);
-  const height = readline.questionFloat('How tall are you? ').toFixed(2);
+async function calculateBMI() {
+  const data = await inquire.prompt([
+    { name: 'weight', type: 'number', message: 'How much do you weight?' },
+    { name: 'height', type: 'number', message: 'How tall are you?' },
+  ]);
+
+  const weight = data.weight;
+  const height = data.height;
 
   console.log('Weight: %s, Height: %s', weight, height);
 
@@ -14,29 +20,28 @@ function findIMC() {
     console.log('Underweight');
     return;
   }
-  
+
   if (bmi >= 18.5 && bmi < 25) {
     console.log('Normal weight');
     return;
   }
-  
+
   if (bmi >= 25 && bmi < 30) {
     console.log('Overweight');
     return;
   }
-  
+
   if (bmi >= 30 && bmi < 35) {
     console.log('Obese');
     return;
   }
-  
+
   if (bmi >= 35 && bmi < 40) {
     console.log('Severely obese');
     return;
   }
-  
-  console.log('Morbidly obese');
 
+  console.log('Morbidly obese');
 }
 
-findIMC();
+calculateBMI();
